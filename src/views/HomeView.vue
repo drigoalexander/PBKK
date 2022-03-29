fd9dc79d6494e1d81a9d4ccc1e37b9f13dfc98d7
drigoalexander <drigosihombinga@gmail.com>
2022-03-29 12:29:06 +0700

    PBKK

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderComp />
+    <HeroComp />
+    <WomanComp />
+    <InstaComp />
+    <PartnerComp />
+    <FooterComp />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderComp from "@/components/HeaderComp.vue";
+import HeroComp from "@/components/HeroComp.vue";
+import WomanComp from "@/components/WomanCom.vue";
+import InstaComp from "@/components/InstaCom.vue";
+import PartnerComp from "@/components/PartnerCom.vue";
+import FooterComp from "@/components/FooterComp.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderComp,
+    HeroComp,
+    WomanComp,
+    InstaComp,
+    PartnerComp,
+    FooterComp,
+  },
+};
+</script>
